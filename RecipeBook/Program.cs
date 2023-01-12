b2eadd18189bed2a1952ecdbf965ce4d1ba8acd5
Seth Clossman <clossmans2@gmail.com>
2023-01-12 12:08:17 -0500

    Added CORS policy and use

--- RecipeBook/Program.cs
+++ RecipeBook/Program.cs
@@ -19,10 +19,22 @@ namespace RecipeBook
                 );
             });
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    policy =>
+                    {
+                        policy.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+
             builder.Services.AddControllers().AddJsonOptions(options =>
             {
                 options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
             });
+            
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -36,10 +48,13 @@ namespace RecipeBook
                 app.UseSwaggerUI();
             }
 
+            
+
             app.UseHttpsRedirection();
 
+            app.UseCors("AllowAll");
+            
             app.UseAuthorization();
-
 
             app.MapControllers();
 
